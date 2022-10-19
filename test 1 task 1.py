d0cba3e524f94d51234254736b2db29eda66642e
simonksimon <113455453+simonksimon@users.noreply.github.com>
2022-10-19 08:30:33 +0200

    Add files via upload

---
+++ test 1 task 1.py
@@ -0,0 +1,11 @@
+Priemer=int(input("Priemer?"))
+Vyska=int(input("Výška?"))
+Polomer=Priemer/2
+#pí*r na druhú
+Dno=3.14*(Polomer*Polomer)
+#2*pí*r*v
+Plast=2*3.14*Polomer*Vyska
+ndDno=Dno*2
+ndPlast=Plast*2
+Spolu=(ndDno+ndPlast)/7.5
+print("Potrebujeme ",Spolu," plechoviek.")
