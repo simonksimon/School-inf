f1a74df6d4750b2f952a0adc91c0dc6a4fd8ad2b
simonksimon <113455453+simonksimon@users.noreply.github.com>
2022-11-08 17:32:34 +0100

    Delete Infdu2 directory

--- Infdu2/infdu while2.py
+++
@@ -1,9 +0,0 @@
-a=0
-b=0
-while b<=100:
-    a=a+1
-    b=b+a
-print(b)
-print(a)
-
-input("To exit, press enter.")
--- Infdu2/infdu while3.py
+++
@@ -1,4 +0,0 @@
-a=(" ")
-while a!=("koniec"):
-    print(a)
-    a=(input("Napíš meno. My ti ho vypíšeme. To exit, press 'koniec'."))
