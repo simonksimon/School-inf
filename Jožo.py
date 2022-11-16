add9dbabf276be98eb5a25303a83d0575a9b60ce
simonksimon <113455453+simonksimon@users.noreply.github.com>
2022-11-16 08:26:26 +0100

    Add files via upload

---
+++ Jožo.py
@@ -0,0 +1,18 @@
+import random
+vstup=[]
+for i in range(10):
+    vstup.append(random.randrange(0,21))
+print(vstup)
+p=0
+n=0
+cisla="0123456789"
+for i in range(len(vstup)):
+    if (vstup[i] % 2)==0:
+        p+=1
+    if (vstup[i] % 2)!=0:
+        n+=1
+if p>n:
+    v=True
+else:
+    v=False
+print(v)
