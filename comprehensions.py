65e82cd8ace4d78f844ddad3f8274f399ff69213
simonksimon <113455453+simonksimon@users.noreply.github.com>
2022-12-21 09:01:53 +0100

    Add files via upload

---
+++ comprehensions.py
@@ -0,0 +1,16 @@
+#zoz=[]
+#for i in range(0,51,2):
+#    zoz.append(i)
+#/
+zoz1=[i for i in range(0,51,2)]
+
+print(zoz1)
+
+#zoz2=[year for year in range(1900,2001) if (year%4==0 and year%100!=0) or year%...]
+#print(zoz2)
+
+#zoz=[(i,j)]...
+
+vec=[-4,-2,0,2,4]
+zoz3=[vec[i] for i in range (len(vec)) if len[i]>0]
+print(zoz3)
